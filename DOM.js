0d21e17c8c2c78d10e5a84a9fba6420670593ce9
viswaFED <100114308+viswaFED@users.noreply.github.com>
2022-02-23 16:11:36 +0530

    javascript

--- DOM.js
+++ DOM.js
@@ -64,25 +64,133 @@
 //  li[i].style.backgroundColor = '#f4f4f4';
 //  }
 
-// QUERYSELECTOR //
-var header = document.querySelector('#main-header');
-header.style.borderBottom = 'solid 4px #ccc';
+// // QUERYSELECTOR //
+// var header = document.querySelector('#main-header');
+// header.style.borderBottom = 'solid 4px #ccc';
 
-var input = document.querySelector('input');
-input.value = 'Hello World'
+// var input = document.querySelector('input');
+// input.value = 'Hello World'
 
-var submit = document.querySelector('input[type="submit"]');
-submit.value="SEND"
+// var submit = document.querySelector('input[type="submit"]');
+// submit.value="SEND"
 
-var item = document.querySelector('.list-group-item');
-item.style.color = 'red';
+// var item = document.querySelector('.list-group-item');
+// item.style.color = 'red';
 
-var lastItem = document.querySelector('.list-group-item:last-child');
-lastItem.style.display = 'none';
+// var lastItem = document.querySelector('.list-group-item:last-child');
+// lastItem.style.display = 'none';
 
-var secondItem = document.querySelector('.list-group-item:nth-child(2)');
-secondItem.style.color = 'coral';
+// var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.color = 'coral';
 
-let thirdItem  = document.querySelector(".list-group-item:nth-child(3)");
-thirdItem.style.backgroundColor = "green";
+// let thirdItem  = document.querySelector(".list-group-item:nth-child(3)");
+// thirdItem.style.backgroundColor = "green";
+// Query Selector ALL
+// var titles = document.querySelectorAll('.title');
+
+// console.log(titles);
+// titles[0].textContent = 'Hello';
+
+// var odd = document.querySelectorAll('li:nth-child(odd)');
+// var even= document.querySelectorAll('li:nth-child(even)');
+
+// for(var i = 0; i < odd.length; i++){
+//   odd[i].style.backgroundColor = 'green';
+//   even[i].style.backgroundColor = '#ccc';
+// }
+
+// var itemList = document.querySelector('#items');
+// //parentNode
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor = '#f4f4f4';
+// console.log(itemList.parentNode.parentNode);
+
+
+// //parentElement
+// console.log(itemList.parentElement);
+// itemList.parentElement.style.backgroundColor = '#f4f4f4';
+// console.log(itemList.parentElement.parentElement.parentElement);
+
+// //childNodes
+// console.log(itemList.childNodes);
+
+// console.log(itemList.children);
+// console.log(itemList.children[1]);
+// itemList.children[1].style.backgroundColor = 'yellow';
+
+// // FirstChild
+// console.log(itemList.firstChild);
+// // firstElementChild
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent = 'Hello 1';
+
+// //lastChild
+// console.log(itemList.lastChild);
+// lastElementChild
+// console.log(itemList.lastElementChild);
+// itemList.lastElementChild.textContent = 'Hello 4';
+
+// //nextSibling
+// console.log(itemList.nextSibling);
+// // nextElementSibling
+// console.log(itemList.nextElementSibling);
+
+// //previousSibling
+// console.log(itemList.previousSibling);
+// previousElementSibling
+// console.log(itemList.previousElementSibling);itemList.previousElementSibling.style.color = 'green';
+
+// // createElement
+
+// // Create a div
+var newDiv =  document.createElement('div');
+
+// Add class
+newDiv.className= 'hello';
+
+// Add id
+newDiv.id = 'hello1';
+
+// Add attr
+newDiv.setAttribute('title', 'Hello Div');
+
+// Create text node
+var newDivText = document.createTextNode('HEllo Word');
+
+// Add text to div
+newDiv.appendChild(newDivText);
+
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+
+console.log(newDiv);
+
+newDiv.style.fontSize = '30px';
+
+container.insertBefore(newDiv, h1);
+// var newDiv =  document.createElement('div');
+
+// // Add class
+// newDiv.className= 'HEllo';
+
+// // Add id
+// newDiv.id = 'HEllo1';
+
+// // Add attr
+// newDiv.setAttribute('title', 'Hello Div1');
+
+// // Create text node
+// var newDivText = document.createTextNode('HEllo word');
+
+// // Add text to div
+// newDiv.appendChild(newDivText);
+
+// var title = document.querySelector('main .title');
+// var h2 = document.querySelector('main h2');
+
+// console.log(newDiv);
+
+// newDiv.style.fontSize = '30px';
+
+// title.insertBefore(newDiv, h2);
 
