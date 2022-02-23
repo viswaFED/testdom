ae798cb7c294be6e815c5d836946311bc46f0ec8
viswaFED <100114308+viswaFED@users.noreply.github.com>
2022-02-23 14:41:55 +0530

    javascript

--- DOM.js
+++ DOM.js
@@ -53,15 +53,36 @@
 //     li[i].style.fontWeight = 'bold';
     
 //    }
-var li = document.getElementsByTagName('li');
-console.log(li);
-console.log(li[1]);
-li[1].textContent = 'Hello 2';
-li[1].style.fontWeight = 'bold';
-li[1].style.backgroundColor = 'yellow';
+// var li = document.getElementsByTagName('li');
+// console.log(li);
+// console.log(li[1]);
+// li[1].textContent = 'Hello 2';
+// li[1].style.fontWeight = 'bold';
+// li[1].style.backgroundColor = 'yellow';
 
-for(var i = 0; i < li.length; i++){
- li[i].style.backgroundColor = '#f4f4f4';
- }
+// for(var i = 0; i < li.length; i++){
+//  li[i].style.backgroundColor = '#f4f4f4';
+//  }
 
+// QUERYSELECTOR //
+var header = document.querySelector('#main-header');
+header.style.borderBottom = 'solid 4px #ccc';
+
+var input = document.querySelector('input');
+input.value = 'Hello World'
+
+var submit = document.querySelector('input[type="submit"]');
+submit.value="SEND"
+
+var item = document.querySelector('.list-group-item');
+item.style.color = 'red';
+
+var lastItem = document.querySelector('.list-group-item:last-child');
+lastItem.style.display = 'none';
+
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.color = 'coral';
+
+let thirdItem  = document.querySelector(".list-group-item:nth-child(3)");
+thirdItem.style.backgroundColor = "green";
 
